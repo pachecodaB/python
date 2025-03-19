12dd31dab82113fce7fe5a3f43ab853cc6eab3fc
Luís Eduardo Pacheco <pacheco.santos.luis@escola.pr.gov.br>
2025-03-19 11:00:32 -0300

    Add files via upload

--- atv18-03/ex29.py
+++ atv18-03/ex29.py
@@ -10,5 +10,39 @@ def cadastrar_cachorro (cadastros):
     idade = input("Idade:")
     doidera = input("Doidera:")
     Demencia = input("Demencia:") 
-    cadastros.append({"Nome":nome, "idade":idade, "Doidera":doidera, "demencia":Demencia})
+    cadastros.append({"Nome":nome, "Idade":idade, "Doidera":doidera, "demencia":Demencia})
     print("Cadastro realizado com sucesso")
+
+def ver_cadastros (cadastros):
+    if not cadastros:
+        print("Nenhum cachorro doido no sistema.")
+    else:
+        print("\n ------- LISTA DE CADASTROS DE DOGS DOIDOS-------")
+
+        for i, pessoa in enumerate (cadastros,1):
+
+            print(f'{i} . nome: {pessoa['nome']}, idade:{pessoa ['idade']}, doidera: {pessoa['doidera']}, demencia: {pessoa['demencia']}')
+                  
+def main():
+    cadastros = []
+    while True:
+        exibir_menu()
+        opção = input("escolha uma opção:")
+            
+        if opção == "1":
+               cadastrar_cachorro(cadastros)
+
+        elif opção == "2":
+               ver_cadastros (cadastros)
+
+        elif opção == "3":
+             print("Obrigado por utilizar o sistema cadastrerdogcrazy")
+             break
+        else:
+             print("Opção inválida! Tente novamente.")
+
+
+if __name__ == "__main__":
+       main()             
+
+               
